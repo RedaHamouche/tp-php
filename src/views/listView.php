a16ec6eeb9f209c1bd5b099395b16f05d73d0763
RedaHamouche <reda.hamouche@outlook.com>
2021-12-02 15:31:59 +0100

    navbar done

--- src/views/listView.php
+++ src/views/listView.php
@@ -1,18 +1,8 @@
 <?php
 $values = FrontEndController::getPost();
+require_once $_SERVER["DOCUMENT_ROOT"]."/views/header.php";
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Document</title>
-</head>
 <body>
-  <a href="./addView">Ajouter un article</a>
-  <a href="./signup">Sign up</a>
 <?php foreach($values as $article) :?>
     <div class="card mx-4 my-3">
         <img src="..." class="card-img-top" alt="...">
